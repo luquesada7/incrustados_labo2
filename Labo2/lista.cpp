aaf42fc974828b28f7f33ad59ba8d8eccc968ef6
luquesada7 <lu.quesada7@gmail.com>
2017-10-13 14:06:08 -0600

    New lista.cpp 13/10/17 14:60

--- Labo2/lista.cpp
+++ Labo2/lista.cpp
@@ -1,64 +1,74 @@
 #include <iostream>
+#include "Strct.hpp"
 using namespace std;
 
-struct mensaje{
-    int TaskID;
-    int Hola;
-};
-
-struct l_stNode{
-    mensaje data;
-    l_stNode *siguiente;
-};
-
-void insertNode(l_stNode *&lista, mensaje nuevoMensaje);
-void eraseList(l_stNode *&lista, mensaje &nuevoMensaje);
+void insertNode(st_Node *&st_pLinkedList, st_Message l_stNewMessage);
+void eraseFirstNode(st_Node *&st_pLinkedList, st_Message &l_stNewMessage);
 
 int main()
 {
-    l_stNode *lista = NULL;
-    mensaje nuevoMensaje;
-    mensaje nuevoMensaje1 = {.TaskID = 1, .Hola = 2};
-    mensaje nuevoMensaje2 = {.TaskID = 2, .Hola = 1};
-    insertNode(lista, nuevoMensaje1);
-    insertNode(lista, nuevoMensaje2);
+    st_Node *st_pLinkedList = NULL;
+    st_Message l_stNewMessage;
+    st_Message l_stNewMessage1 = {.std_u8DestTaskID = 1, .std_u16IntData = 2};
+    st_Message l_stNewMessage2 = {.std_u8DestTaskID = 2, .std_u16IntData = 1};
+    insertNode(st_pLinkedList, l_stNewMessage1);
+    insertNode(st_pLinkedList, l_stNewMessage2);
 
-    while(lista != NULL)
+    while(st_pLinkedList != NULL)
     {
-        eraseList(lista, nuevoMensaje);
-        cout<<nuevoMensaje.TaskID<< " -> ";
+        eraseFirstNode(st_pLinkedList, l_stNewMessage);
+        cout<<l_stNewMessage.std_u8DestTaskID<< " -> ";
     }
+
     return 0;
 }
 
-void insertNode(l_stNode *&lista, mensaje nuevoMensaje){
-    l_stNode *l_stNewNode = new l_stNode();
-    l_stNewNode->data = nuevoMensaje;
+// ##############################################
+// Function that inserts new node on linked list
+// - creates new node
+// - saves message structure to node
+// - links node to list ordering them in ascending
+//   order with the DestTaskID of the message
+// ##############################################
 
-    l_stNode *aux1 = lista;
-    l_stNode *aux2;
+void insertNode(st_Node *&st_pLinkedList, st_Message l_stNewMessage){
+    st_Node *l_stNewNode = new st_Node();
+    l_stNewNode->std_stMssg = l_stNewMessage;
 
-    while((aux1 != NULL) && (aux1->data.TaskID < nuevoMensaje.TaskID)){
-        aux2 = aux1;
-        aux1 = aux1->siguiente;
+    st_Node *l_stAux1 = st_pLinkedList;
+    st_Node *l_stAux2;
+
+    // -while list is not empty and DestTaskID of first node is smaller than the new message DestTaskID
+    while((l_stAux1 != NULL) && (l_stAux1->std_stMssg.std_u8DestTaskID < l_stNewMessage.std_u8DestTaskID)){
+        l_stAux2 = l_stAux1; //- points to first node
+        l_stAux1 = l_stAux1->std_pnext; //- new position
     }
 
-    if(lista == aux1)
+    if(st_pLinkedList == l_stAux1)
     {
-        lista = l_stNewNode;
+        st_pLinkedList = l_stNewNode; //- adding node at the beggining of the list
     }else
     {
-        aux2->siguiente = l_stNewNode;
+        l_stAux2->std_pnext = l_stNewNode; // -adding node in the middle or back of the list
     }
 
-    l_stNewNode->siguiente = aux1;
+    l_stNewNode->std_pnext = l_stAux1;
 
-    cout<<"\tElemento con TaskID "<<nuevoMensaje.TaskID<<" y Hola "<<nuevoMensaje.Hola<<" insertado a lista correctamente\n";
+    cout<<"\tElemento con std_u8DestTaskID "<<l_stNewMessage.std_u8DestTaskID<<" y std_u16IntData "<<l_stNewMessage.std_u16IntData<<" insertado a st_pLinkedList correctamente\n";
 }
 
-void eraseList(l_stNode *&lista, mensaje &nuevoMensaje){
-    l_stNode *aux = lista;
-    nuevoMensaje = aux->data;
-    lista = aux->siguiente;
-    delete aux;
+// ##############################################
+// Function that erases a node on a linked list
+// - Creates a temporary node to store the node
+//   on the list that will be deleted
+// - Pointer to list then points to the second
+//   node
+// - Temporary node is deleted
+// ##############################################
+
+void eraseFirstNode(st_Node *&st_pLinkedList, st_Message &l_stMessage){
+    st_Node *l_stAux = st_pLinkedList;
+    l_stMessage = l_stAux->std_stMssg;
+    st_pLinkedList = l_stAux->std_pnext;
+    delete l_stAux;
 }
