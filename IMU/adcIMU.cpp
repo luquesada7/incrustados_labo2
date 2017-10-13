e40f2bd1c0a3da1b0aec7a0111f5d25ff6b1c7e0
luquesada7 <lu.quesada7@gmail.com>
2017-10-12 22:49:30 -0600

    new adcIMU

--- IMU/adcIMU.cpp
+++ IMU/adcIMU.cpp
@@ -3,54 +3,107 @@
 #include <C:/ti/simplelink_msp432_sdk_1_40_01_00/source/ti/grlib/grlib.h>
 #include "LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h"
 #include "LcdDriver/Crystalfontz128x128_ST7735.h"
-
-#define PENDIENTE 2.482424242
+#include <stdlib.h>
 
 /* Graphic library context */
 Graphics_Context g_sContext;
 
-// Holalalallal
-//
-//
-// :)
-//
-
-//COMENTARIOOOOOOOOOOOOOOOOOOOOOOOO
-
-// Hola Jose!
-
-// CONFLICTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!! :-(
-
-//
-
-//
-
-
-// Me llamo Luisai
-
-// Hola Luluisaaa, qué tal te va!? 
-// Nuevo
-// Comentario
-uint16_t ADC14Result = 0U;
+//uint16_t ADC14Result = 0U;
 uint16_t ADC14Resultx = 0U;
 uint16_t ADC14Resulty = 0U;
 uint16_t ADC14Resultz = 0U;
 int numlines = 0U;
 double linesLCD = 0U;
+
+float m_fPastZ = 7500;
+float m_fNewZ = 4500;
+float m_fDelta;
+int m_iLines;
+int m_iNewLine;
+bool m_bColor;
+const float l_fPixDelta = 128.0 / 7000.0;
+const int l_iPixLine = 128;
+
+int m_iLastLine = 64;
+int m_iPixels;
+uint16_t l_uint16tulValue;
+Graphics_Rectangle l_stRect;
+
 void Setup(void);
 void drawAccelData(int num);
+
 int main(void)
 {
-
     Setup();
     while (1)
     {
-        linesLCD = (ADC14Resultz*PENDIENTE - 4800*PENDIENTE)/128;
-        numlines = static_cast<int>(linesLCD);
+        //linesLCD = (ADC14Resultz*PENDIENTE - 4800*PENDIENTE)/128;
+        //numlines = static_cast<int>(linesLCD);
+        //drawAccelData(numlines);
 
-        drawAccelData(numlines);
+        if (m_fNewZ > m_fPastZ)
+        {
+            m_bColor = true;
+        }
+        else
+        {
+            m_bColor = false;
+        }
+
+        //################################
+        // Calculating number of pixels
+        // that need to be painted
+        //################################
+        m_fDelta = abs(m_fNewZ - m_fPastZ);
+        m_iLines = 1
+                + ((m_fDelta * l_fPixDelta * l_iPixLine - l_iPixLine)
+                        / l_iPixLine);
+
+        l_stRect.xMin = 0;
+        l_stRect.xMax = 127;
+
+        if (m_bColor)
+        {
+            m_iNewLine = m_iLastLine - m_iLines;
+            if (m_iNewLine < 0)
+            {
+                l_stRect.yMin = 0;
+                l_stRect.yMax = m_iLastLine;
+            }
+            else
+            {
+                l_stRect.yMin = m_iNewLine;
+                l_stRect.yMax = m_iLastLine;
+            }
+
+            l_uint16tulValue = 0XAA00; //brown
+
+            Graphics_fillRectangleOnDisplay(&g_sCrystalfontz128x128, &l_stRect,
+                                            l_uint16tulValue);
+        }
+        else
+        {
+            m_iNewLine = m_iLastLine + m_iLines;
+
+            if (m_iNewLine > 128)
+            {
+                l_stRect.yMin = m_iLastLine;
+                l_stRect.yMax = 127;
+            }
+            else
+            {
+                l_stRect.yMin = m_iLastLine;
+                l_stRect.yMax = m_iNewLine;
+            }
+
+            l_uint16tulValue = 0X02FF; //blue
+
+            Graphics_fillRectangleOnDisplay(&g_sCrystalfontz128x128, &l_stRect,
+                                            l_uint16tulValue);
+        }
+
     }
-   // return 0;
+// return 0;
 }
 
 void Setup(void)
@@ -59,11 +112,10 @@ void Setup(void)
 
     P1->DIR = BIT0;
     P1->OUT = BIT0;
-    // Set P4.3 for Analog input, disabling the I/O circuit.
+// Set P4.3 for Analog input, disabling the I/O circuit.
     P4->SEL0 = BIT3;
     P4->SEL1 = BIT3;
     P4->DIR &= ~BIT3;
-
 
     /* Configures Pin 4.0, 4.2, and 6.1 as ADC input */
     P4->SEL0 = BIT0;
@@ -76,7 +128,7 @@ void Setup(void)
     P6->SEL1 = BIT1;
     P6->DIR &= ~BIT1;
 
-    //TIMER32_1->LOAD = 0x00B71B00; //~0.5s ---> a 48Mhz
+//TIMER32_1->LOAD = 0x00B71B00; //~0.5s ---> a 48Mhz
     TIMER32_1->LOAD = 0x0000BB80; //~0.5s ---> a 48Mhz
     TIMER32_1->CONTROL =
     TIMER32_CONTROL_SIZE | TIMER32_CONTROL_PRESCALE_0 | TIMER32_CONTROL_MODE
@@ -87,13 +139,14 @@ void Setup(void)
     ADC14->CTL0 = ADC14_CTL0_PDIV_0 | ADC14_CTL0_SHS_0 | ADC14_CTL0_DIV_7 |
     ADC14_CTL0_SSEL__MCLK | ADC14_CTL0_SHT0_1 | ADC14_CTL0_ON | ADC14_CTL0_SHP
             | ADC14_CTL0_CONSEQ_3;
-    ADC14->MCTL[0] = ADC14_MCTLN_INCH_10 | ADC14_MCTLN_VRSEL_0;
+
+//ADC14->MCTL[0] = ADC14_MCTLN_INCH_10 | ADC14_MCTLN_VRSEL_0;
     ADC14->MCTL[1] = ADC14_MCTLN_INCH_11 | ADC14_MCTLN_VRSEL_0;
     ADC14->MCTL[2] = ADC14_MCTLN_INCH_13 | ADC14_MCTLN_VRSEL_0;
     ADC14->MCTL[3] = ADC14_MCTLN_INCH_14 | ADC14_MCTLN_VRSEL_0;
+
     ADC14->CTL0 = ADC14->CTL0 | ADC14_CTL0_ENC;
-    ADC14->IER0 = ADC14_IER0_IE0 | ADC14_IER0_IE1 | ADC14_IER0_IE2
-            | ADC14_IER0_IE3;
+    ADC14->IER0 = ADC14_IER0_IE1 | ADC14_IER0_IE2 | ADC14_IER0_IE3;
     NVIC_SetPriority(ADC14_IRQn, 1);
     NVIC_EnableIRQ(ADC14_IRQn);
 
@@ -104,10 +157,38 @@ void Setup(void)
     Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
 
     /* Initializes graphics context */
-    Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128, &g_sCrystalfontz128x128_funcs);
+    Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128,
+                         &g_sCrystalfontz128x128_funcs);
     Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_RED);
     Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
     GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
+
+    /*
+     * Paint initial display
+     */
+
+    Graphics_Rectangle l_stRectInit;
+
+    uint16_t l_uint16tValue;
+
+    l_stRectInit.xMin = 0;
+    l_stRectInit.xMax = 127;
+    l_stRectInit.yMin = 0;
+    l_stRectInit.yMax = 63;
+
+    l_uint16tValue = 0X00FF; //blue
+
+    Graphics_fillRectangleOnDisplay(&g_sCrystalfontz128x128, &l_stRectInit,
+                                    l_uint16tValue);
+    l_stRectInit.xMin = 0;
+    l_stRectInit.xMax = 127;
+    l_stRectInit.yMin = 64;
+    l_stRectInit.yMax = 127;
+
+    l_uint16tValue = 0XAA00; //brown
+
+    Graphics_fillRectangleOnDisplay(&g_sCrystalfontz128x128, &l_stRectInit,
+                                    l_uint16tValue);
 
 }
 
@@ -115,46 +196,46 @@ void drawAccelData(int num)
 {
     HAL_LCD_writeCommand(CM_RAMWR);
     int i;
-    for (i = 0; i < num*128 ; i++)
+    for (i = 0; i < num * 128; i++)
     {
-        HAL_LCD_writeData(0x36);
+        HAL_LCD_writeData(0xAA);
         //light blue
-        HAL_LCD_writeData(0x36);
+        HAL_LCD_writeData(0xAA);
     }
     int j;
-    for (j = num*128 ; j < 16384; j++ )
+    for (j = num * 128; j < 16384; j++)
     {
-        HAL_LCD_writeData(0XAA);
+        HAL_LCD_writeData(0X36);
 //brown
-        HAL_LCD_writeData(0XAA);
+        HAL_LCD_writeData(0X36);
     }
 
-    HAL_LCD_delay(10);
+//HAL_LCD_delay(10000);
     HAL_LCD_writeCommand(CM_DISPON);
 }
 
 extern "C"
 {
-    void T32_INT1_IRQHandler(void)
-    {
-        __disable_irq();
-        TIMER32_1->INTCLR = 0U;
-        P1->OUT ^= BIT0;
-        ADC14->CTL0 = ADC14->CTL0 | ADC14_CTL0_SC; // Start
-        __enable_irq();
-        return;
-    }
+void T32_INT1_IRQHandler(void)
+{
+    __disable_irq();
+    TIMER32_1->INTCLR = 0U;
+    P1->OUT ^= BIT0;
+    ADC14->CTL0 = ADC14->CTL0 | ADC14_CTL0_SC; // Start
+    __enable_irq();
+    return;
+}
 
-    void ADC14_IRQHandler(void)
-    {
-        __disable_irq();
-        ADC14Result = ADC14->MEM[0];
-        ADC14Resultz = ADC14->MEM[1];
-        ADC14Resulty = ADC14->MEM[2];
-        ADC14Resultx = ADC14->MEM[3];
-        ADC14->CLRIFGR0 = ADC14_CLRIFGR0_CLRIFG0 | ADC14_CLRIFGR0_CLRIFG1
-                | ADC14_CLRIFGR0_CLRIFG2 | ADC14_CLRIFGR0_CLRIFG3;
-        __enable_irq();
-        return;
-    }
+void ADC14_IRQHandler(void)
+{
+    __disable_irq();
+//ADC14Result = ADC14->MEM[0];
+    ADC14Resultz = ADC14->MEM[1];
+    ADC14Resulty = ADC14->MEM[2];
+    ADC14Resultx = ADC14->MEM[3];
+    ADC14->CLRIFGR0 = ADC14_CLRIFGR0_CLRIFG1 | ADC14_CLRIFGR0_CLRIFG2
+            | ADC14_CLRIFGR0_CLRIFG3;
+    __enable_irq();
+    return;
+}
 }
